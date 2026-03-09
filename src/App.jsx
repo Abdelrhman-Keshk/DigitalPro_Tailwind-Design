c8bc2557661782b47ee9ee2035be9d46ed48eccb
Abdelrhmankeshk <abdelrhmankeshk15@gmail.com>
2026-03-09 23:21:58 +0200

    improve responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { Navbar, Hero, Services, OurWork, Team, Footer } from "./components"
 
 export default function App() {
   return (
-    <div className="App bg-gray-50 min-h-screen box-border">
-      <Navbar/>
-      <Hero/>
-      <Services/>
-      <OurWork/>
-      <Team/>
-      <Footer/>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <Services />
+      <OurWork />
+      <Team />
+      <Footer />
+    </>
   )
 }
